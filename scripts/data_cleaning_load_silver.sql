4bfd2d5913ad91b029455fc575d8c5066afa9f6d
Bitan Sarkar <93920114+BitanXD@users.noreply.github.com>
2026-01-01 22:34:36 +0530

    Modify crm_prd_info insertion with date casting
    Updated SQL to insert data into crm_prd_info with new date calculations.

--- scripts/data_cleaning_load_silver.sql
+++ scripts/data_cleaning_load_silver.sql
@@ -33,9 +33,19 @@ WHERE flag_last = 1;
 
 -- crm_prd_info
 
+INSERT INTO dw_silver.crm_prd_info (
+prd_id,
+cat_id,
+prd_key,
+prd_nm,
+prd_cost,
+prd_line,
+prd_start_dt,
+prd_end_dt
+)
+
 SELECT 
 prd_id,
-prd_key,
 REPLACE(SUBSTRING(prd_key, 1, 5),'-','_') AS cat_id,
 SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_key,
 prd_nm,
@@ -47,6 +57,6 @@ CASE UPPER(TRIM(prd_line))
 	 WHEN 'T' THEN 'Touring'
 	 ELSE 'N/A'
 END AS prd_line,
-prd_start_dt,
-prd_end_dt
+CAST (prd_start_dt AS DATE) AS prd_start_dt,
+CAST (LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt) - 1 AS DATE) AS prd_end_dt
 FROM dw_bronze.crm_prd_info;
