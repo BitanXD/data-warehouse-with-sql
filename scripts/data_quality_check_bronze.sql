3f0db18be059e61cdca4c286ae5594708e93b029
Bitan Sarkar <93920114+BitanXD@users.noreply.github.com>
2026-01-01 21:04:40 +0530

    Add data quality checks for crm_prd_info table
    Added data quality checks for crm_prd_info table including checks for nulls, duplicates, unwanted spaces, negative numbers, and invalid date orders.

--- scripts/data_quality_check_bronze.sql
+++ scripts/data_quality_check_bronze.sql
@@ -24,3 +24,37 @@ FROM dw_bronze.crm_cust_info;
 
 SELECT DISTINCT cst_marital_status
 FROM dw_bronze.crm_cust_info;
+
+
+-- crm_prd_info
+
+-- Check for nulls or duplicates in Primary Key...
+-- Expectation: No result
+
+SELECT prd_id, COUNT(*) 
+FROM dw_bronze.crm_prd_info
+GROUP BY prd_id
+HAVING COUNT(*) > 1 OR prd_id IS NULL;
+
+-- Check for unwanted spaces...
+-- Expectation: No result
+SELECT prd_nm
+FROM dw_bronze.crm_prd_info
+WHERE prd_nm != TRIM(prd_nm);
+
+
+-- Check for Null or Negative numbers...
+-- Expectation: No result
+SELECT prd_cost
+FROM dw_bronze.crm_prd_info
+WHERE prd_cost IS NULL OR prd_cost < 0;
+
+-- Data Standardization and Consistency
+SELECT DISTINCT prd_line
+FROM dw_bronze.crm_prd_info;
+
+
+-- Check for invalid date orders
+SELECT *
+FROM dw_bronze.crm_prd_info
+WHERE prd_end_dt < prd_start_dt;
