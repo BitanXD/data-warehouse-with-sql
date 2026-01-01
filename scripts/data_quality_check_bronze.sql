732e21721460d1d5771eda4460e6b21e51a90a4b
Bitan Sarkar <93920114+BitanXD@users.noreply.github.com>
2026-01-01 19:40:49 +0530

    Add data quality check script for crm_cust_info
    This SQL script checks for data quality issues in the crm_cust_info table, including nulls, duplicates, and unwanted spaces in customer names.

---
+++ scripts/data_quality_check_bronze.sql
@@ -0,0 +1,26 @@
+-- crm_cust_info
+
+-- Check for nulls or duplicates in Primary Key...
+-- Expectation: No result
+
+SELECT cst_id, COUNT(*) 
+FROM dw_bronze.crm_cust_info
+GROUP BY cst_id
+HAVING COUNT(*) > 1 OR cst_id IS NULL;
+
+-- Check for unwanted spaces...
+-- Expectation: No result
+SELECT cst_firstname
+FROM dw_bronze.crm_cust_info
+WHERE cst_firstname != TRIM(cst_firstname);
+
+SELECT cst_lastname
+FROM dw_bronze.crm_cust_info
+WHERE cst_lastname != TRIM(cst_lastname);
+
+-- Data Standardization and Consistency
+SELECT DISTINCT cst_gndr
+FROM dw_bronze.crm_cust_info;
+
+SELECT DISTINCT cst_marital_status
+FROM dw_bronze.crm_cust_info;
