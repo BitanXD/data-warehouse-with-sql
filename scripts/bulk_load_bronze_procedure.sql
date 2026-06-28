11c92b65d3cade277ea04b66b23a7c5f2345e9bf
Bitan Sarkar <93920114+BitanXD@users.noreply.github.com>
2026-06-28 18:12:00 +0530

    Update file paths for bulk insert operations

--- scripts/bulk_load_bronze_procedure.sql
+++ scripts/bulk_load_bronze_procedure.sql
@@ -15,7 +15,7 @@ DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @b
 		TRUNCATE TABLE dw_bronze.crm_cust_info;
 		PRINT '>> Inserting data into table: dw_bronze.crm_cust_info';
 		BULK INSERT dw_bronze.crm_cust_info
-		FROM 'C:\Users\bitan\OneDrive\Documents\Datasets\datasets\source_crm\cust_info.csv'
+		FROM 'E:\Data WareHouse\data-warehouse-with-sql\datasets\source_crm\cust_info.csv'
 		WITH (
 		FIRSTROW = 2,
 		FIELDTERMINATOR = ',',
@@ -31,7 +31,7 @@ DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @b
 		TRUNCATE TABLE dw_bronze.crm_prd_info;
 		PRINT '>> Inserting data into table: dw_bronze.crm_prd_info';
 		BULK INSERT dw_bronze.crm_prd_info
-		FROM 'C:\Users\bitan\OneDrive\Documents\Datasets\datasets\source_crm\prd_info.csv'
+		FROM 'E:\Data WareHouse\data-warehouse-with-sql\datasets\source_crm\prd_info.csv'
 		WITH (
 		FIRSTROW = 2,
 		FIELDTERMINATOR = ',',
@@ -47,7 +47,7 @@ DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @b
 		TRUNCATE TABLE dw_bronze.crm_sales_details;
 		PRINT '>> Inserting data into table: dw_bronze.crm_sales_details';
 		BULK INSERT dw_bronze.crm_sales_details
-		FROM 'C:\Users\bitan\OneDrive\Documents\Datasets\datasets\source_crm\sales_details.csv'
+		FROM 'E:\Data WareHouse\data-warehouse-with-sql\datasets\source_crm\sales_details.csv'
 		WITH (
 		FIRSTROW = 2,
 		FIELDTERMINATOR = ',',
@@ -67,7 +67,7 @@ DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @b
 		TRUNCATE TABLE dw_bronze.erp_cust_az12;
 		PRINT '>> Inserting data into table: dw_bronze.erp_cust_az12';
 		BULK INSERT dw_bronze.erp_cust_az12
-		FROM 'C:\Users\bitan\OneDrive\Documents\Datasets\datasets\source_erp\CUST_AZ12.csv'
+		FROM 'E:\Data WareHouse\data-warehouse-with-sql\datasets\source_erp\CUST_AZ12.csv'
 		WITH (
 		FIRSTROW = 2,
 		FIELDTERMINATOR = ',',
@@ -83,7 +83,7 @@ DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @b
 		TRUNCATE TABLE dw_bronze.erp_loc_a101;
 		PRINT '>> Inserting data into table: dw_bronze.erp_loc_a101';
 		BULK INSERT dw_bronze.erp_loc_a101
-		FROM 'C:\Users\bitan\OneDrive\Documents\Datasets\datasets\source_erp\LOC_A101.csv'
+		FROM 'E:\Data WareHouse\data-warehouse-with-sql\datasets\source_erp\LOC_A101.csv'
 		WITH (
 		FIRSTROW = 2,
 		FIELDTERMINATOR = ',',
@@ -99,7 +99,7 @@ DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @b
 		TRUNCATE TABLE dw_bronze.erp_px_cat_g1v2;
 		PRINT '>> Inserting data into table: dw_bronze.erp_px_cat_g1v2';
 		BULK INSERT dw_bronze.erp_px_cat_g1v2
-		FROM 'C:\Users\bitan\OneDrive\Documents\Datasets\datasets\source_erp\PX_CAT_G1V2.csv'
+		FROM 'E:\Data WareHouse\data-warehouse-with-sql\datasets\source_erp\PX_CAT_G1V2.csv'
 		WITH (
 		FIRSTROW = 2,
 		FIELDTERMINATOR = ',',
